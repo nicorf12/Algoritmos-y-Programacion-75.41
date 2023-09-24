47ded1aac9e0cb5e9c18282a895e1b43e21afa8f
Nicolas.R <133537447+nicorf12@users.noreply.github.com>
2023-09-23 23:23:37 -0300

    Update main.go

--- hello_world/main.go
+++ hello_world/main.go
@@ -5,3 +5,52 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+///MERGE SORT///
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	middle := len(arr) / 2
+	left := make([]int, middle)
+	right := make([]int, len(arr)-middle)
+
+	copy(left, arr[:middle])
+	copy(right, arr[middle:])
+
+	left = mergeSort(left)
+	right = mergeSort(right)
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, len(left)+len(right))
+	i, j, k := 0, 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result[k] = left[i]
+			i++
+		} else {
+			result[k] = right[j]
+			j++
+		}
+		k++
+	}
+
+	for i < len(left) {
+		result[k] = left[i]
+		i++
+		k++
+	}
+
+	for j < len(right) {
+		result[k] = right[j]
+		j++
+		k++
+	}
+
+	return result
+}
+///////////////
